50f518b97da820fb2298dc6f18c21bb68e4f0c3c
lvsheng <lvsheng@baidu.com>
2015-11-07 00:34:25 +0800

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by lvsheng on 15/11/7.
+//  Copyright © 2015年 lvsheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
