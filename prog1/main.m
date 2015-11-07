b909ae6cfb6eb4e83d46537708ee3c877c8848d7
lvsheng <lvsheng@baidu.com>
2015-11-08 00:35:46 +0800

    增加SEL、static成员、%@的尝试

--- prog1/main.m
+++ prog1/main.m
@@ -18,10 +18,14 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Fraction *f = [[Fraction alloc] init];
-        [f setNumberator: 1];
-        [f setDenominator: 3];
-        [f print];
+        Fraction *fraction = [[Fraction alloc] init];
+        [fraction setNumberator: 1];
+        [fraction setDenominator: 3];
+
+        SEL sel = @selector(print);
+        NSLog(@"SEL: %s", sel); //try log SEL as a char*, and will log the function name "print"
+        [fraction print];
+        [fraction performSelector:sel]; //equal to [fraction print], the performSelector will transform SEL to id
     }
     return 0;
 }
@@ -32,9 +36,12 @@ int main(int argc, const char * argv[]) {
     int denominator;
 }
 
+static Fraction* sInstance = 0; //define a static member
+
 -(void) print
 {
     NSLog(@"%i/%i", numberator, denominator);
+    NSLog(@"sInstance: %@", sInstance); //%@ will log the object, on this is null
 }
 
 -(void) setNumberator: (int) n
