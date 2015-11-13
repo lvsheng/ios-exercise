29eff0ebd155e75be39891a7d0d03fab24ac1fc6
lvsheng <lvsheng@baidu.com>
2015-11-13 23:13:41 +0800

    增加sel,imp performSelector,methodForSelector等

--- prog1/main.m
+++ prog1/main.m
@@ -18,14 +18,51 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        //指针
+        int *nP = alloca(sizeof(int));
+        *nP = 3;
+        NSLog(@"nP: %p, *nP: %d", nP, *nP);
+
+        //NSNumber
+        NSNumber *n = @3;
+        NSLog(@"hello, %@", n);
+
+        //class
         Fraction *fraction = [[Fraction alloc] init];
         [fraction setNumberator: 1];
         [fraction setDenominator: 3];
 
-        SEL sel = @selector(print);
-        NSLog(@"SEL: %s", sel); //try log SEL as a char*, and will log the function name "print"
+        //sel
+        NSLog(@"sel");
+        SEL sel = NSSelectorFromString(@"print");
         [fraction print];
-        [fraction performSelector:sel]; //equal to [fraction print], the performSelector will transform SEL to id
+        if (!fraction) {
+            [NSException raise:@"fraction has no value" format:@"fraction is %@", fraction];
+        }
+        if (![fraction respondsToSelector: sel]) {
+            [NSException raise:@"not existed sel" format:@"sel %s is not exist", sel_getName(sel)];
+        }
+        [fraction performSelector: sel]; //equal to [fraction print], the performSelector will call the method that sel select
+        
+        //imp
+        NSLog(@"imp");
+        IMP imp = [fraction methodForSelector: sel];
+        void (*func)(id, SEL) = (void *)imp;
+        func(fraction, sel);
+        //TODO: 为什么methodForSelector都需要sel参数呢？而且试验里就算给methodForSelector传一个不存在的sel也可以得到一个可调用的func，在执行func时才抛出错误（即便func调用时传入正确的sel也仍抛错）
+        //而如下面调用func时传入一个错误的sel却可以正常调用
+        func(fraction, NSSelectorFromString(@"testasdlfkajsdf"));
+
+        SEL sel2 = NSSelectorFromString(@"getResult");
+        float (*func2)(id, SEL) = (void *)[fraction methodForSelector:sel2];
+        NSLog(@"getResult: %f", func2(fraction, sel2));
+        
+        //SEL sel3 = NSSelectorFromString(@"setNumberatorWithNSNumber");
+        //void (*func3)(id, SEL, int) = (void *)[fraction methodForSelector:sel3];
+        //func3(fraction, sel3, 3); //调用会报错。。。说unrecognized selector。。。。 不知道为什么。。。
+        //NSNumber *n3 = @3;
+        //[fraction performSelector:sel3 withObject:n3];
+        [fraction print];
     }
     return 0;
 }
@@ -42,6 +79,9 @@ static Fraction* sInstance = 0; //define a static member
 {
     NSLog(@"%i/%i", numberator, denominator);
     NSLog(@"sInstance: %@", sInstance); //%@ will log the object, on this is null
+    NSLog(@"sInstance: %g", [self getResult]);
+    SEL sel = NSSelectorFromString(@"getResult");
+        NSLog(@"result: %d", [self performSelector:sel]);
 }
 
 -(void) setNumberator: (int) n
@@ -49,9 +89,19 @@ static Fraction* sInstance = 0; //define a static member
     numberator = n;
 }
 
+-(void) setNumberatorWithNSNumber:(NSNumber*)n
+{
+    [self setNumberator:[n intValue]];
+}
+
 -(void) setDenominator: (int) d
 {
     denominator = d;
+}
+
+-(float) getResult
+{
+    return (float)numberator / denominator;
 }
 
 @end
